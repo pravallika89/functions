5759f7641a66a55af5459f4a1ec6d7d49bf6b993
pravallika <pravallikaammineni@github.com>
2019-05-06 20:36:10 -0700

    hoisting

---
+++ js/hoisting.js
@@ -0,0 +1,10 @@
+funcDeclaration();
+
+function funcDeclaration(){
+    console.log('i'm inside function declaration');
+}
+
+// funcExpression();
+var funcExpression= function(){
+    console.log('I'm inside function expression');
+};
